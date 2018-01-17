3b3faad2d62c818135232fd4666ecfd2388b43cf
Griff Green <saveoursonics@gmail.com>
2018-01-17 18:37:55 +0700

    Made comments more like a readme & Formating
    I revised the comments a lot but only to match the Giveth style, the comments were great, which ever one of you wrote these, great work!
    
    Also made small formatting changes, moving the `)`to the second line and maybe some other random changes?

--- contracts/MultiSend.sol
+++ contracts/MultiSend.sol
@@ -1,3 +1,5 @@
+pragma solidity ^0.4.18;
+
 // Copyright (C) 2018 Alon Bukai This program is free software: you 
 // can redistribute it and/or modify it under the terms of the GNU General 
 // Public License as published by the Free Software Foundation, version. 
@@ -7,24 +9,25 @@
 // more details. You should have received a copy of the GNU General Public
 // License along with this program. If not, see http://www.gnu.org/licenses/
 
-pragma solidity ^0.4.18;
-
 import "giveth-common-contracts/contracts/Escapable.sol";
 
-/// @notice `MultiSend` is a contract for sending multiple ETH/ERC20 Tokens to
-///  multiple addresses. In addition this contract can call multiple contracts
-///  with multiple amounts. There are also TightlyPacked functions which in
-///  some situations allow for gas savings. TightlyPacked is cheaper if you
-///  need to store input data and if amount is less than 12 bytes. Normal is
-///  cheaper if you don't need to store input data or if amounts are greater
-///  than 12 bytes. Supports deterministic deployment. As explained
-///  here: https://github.com/ethereum/EIPs/issues/777#issuecomment-356103528
+/// @notice `MultiSend` is a contract for making multiple ETH/ERC20 Token sends
+///  to multiple addresses and calling multiple contracts with multiple amounts; 
+///  there are TightlyPacked functions which often allow for gas savings if
+///  you need to store input data and the `amount` being sent is less than 12 
+///  bytes (0xffffffffffff); supports deterministic deployment, explained here:
+///  https://github.com/ethereum/EIPs/issues/777#issuecomment-356103528
 contract MultiSend is Escapable {
   
-    /// @dev Hardcoded escapeHatchCaller
+    /// @dev Hardcoded escapeHatchCaller to Griff Green's DAO Curator Address
     address CALLER = 0x839395e20bbB182fa440d08F850E6c7A8f6F0780;
-    /// @dev Hardcoded escapeHatchDestination
+
+    /// @dev Hardcoded escapeHatchDestination to the WHG's Multisig 
     address DESTINATION = 0x8ff920020c8ad673661c8117f2855c384758c572;
+
+//////////
+// Events
+//////////
 
     event MultiTransfer(
         address indexed _from,
@@ -48,22 +51,30 @@ contract MultiSend is Escapable {
         ERC20 _token
     );
 
-    /// @notice Constructor using Escapable and Hardcoded values
+///////////////
+// Constructor
+///////////////
+
+    /// @notice Constructor using Escapable with Hardcoded values
     function MultiSend() Escapable(CALLER, DESTINATION) public {}
 
-    /// @notice Send to multiple addresses using a byte32 array which
-    ///  includes the address and the amount.
-    ///  Addresses and amounts are stored in a packed bytes32 array
-    ///  Address is stored in the 20 most significant bytes
-    ///  The address is retrieved by bitshifting 96 bits to the right
-    ///  Amount is stored in the 12 least significant bytes
-    ///  The amount is retrieved by taking the 96 least significant bytes
-    ///  and converting them into an unsigned integer
-    ///  Payable
-    /// @param _addressesAndAmounts Bitwise packed array of addresses
-    ///  and amounts
-    function multiTransferTightlyPacked(bytes32[] _addressesAndAmounts)
-    payable public returns(bool)
+///////////////////
+// Ether Functions
+///////////////////
+
+
+    /// @notice Most efficient function for Multisigs and other Contracts
+    ///  to use; Allows you to send ether to multiple addresses by attaching a
+    ///  byte32 array to the total ETH to be sent; the data includes the 
+    ///  receiving address & the amount to be sent stored in a packed bytes32
+    ///  array where the address is in the 20 leftmost bytes, retrieved by
+    ///  bitshifting it 96 bits to the right, and the amount is stored in the 12
+    ///  rightmost bytes, retrieved by taking the 96 rightmost bytes and
+    ///  converting them into an unsigned integer
+    /// @param _addressesAndAmounts Bitwise packed array of addresses and
+    ///  amounts (denominated in wei) detailing the desired transfers
+    function multiTransferTightlyPacked(bytes32[] _addressesAndAmounts
+    )payable public returns(bool)
     {
         uint startBalance = this.balance;
         for (uint i = 0; i < _addressesAndAmounts.length; i++) {
@@ -72,40 +83,40 @@ contract MultiSend is Escapable {
             _safeTransfer(to, amount);
             MultiTransfer(msg.sender, msg.value, to, amount);
         }
-        require(startBalance - msg.value == this.balance);
+        require(startBalance - msg.value == this.balance); // Accounting check
         return true;
     }
 
-    /// @notice Send to multiple addresses using two arrays which
-    ///  includes the address and the amount.
-    ///  Payable
-    /// @param _addresses Array of addresses to send to
-    /// @param _amounts Array of amounts to send
-    function multiTransfer(address[] _addresses, uint[] _amounts)
-    payable public returns(bool)
+    /// @notice Most efficient function when sending from a normal account; 
+    ///  Allows you to send ETH to multiple addresses by attaching two arrays of
+    ///  data, one array for the addresses and one array for the amounts
+    /// @param _addresses Array of addresses to receive ETH
+    /// @param _amounts Array of amounts (in wei) to send to each address
+    function multiTransfer(address[] _addresses, uint[] _amounts
+    )payable public returns(bool)
     {
         uint startBalance = this.balance;
         for (uint i = 0; i < _addresses.length; i++) {
             _safeTransfer(_addresses[i], _amounts[i]);
             MultiTransfer(msg.sender, msg.value, _addresses[i], _amounts[i]);
         }
-        require(startBalance - msg.value == this.balance);
+        require(startBalance - msg.value == this.balance); // Accounting check
         return true;
     }
 
-    /// @notice Call to multiple contracts using a byte32 array which
-    ///  includes the contract address and the amount.
-    ///  Addresses and amounts are stored in a packed bytes32 array.
-    ///  Address is stored in the 20 most significant bytes.
-    ///  The address is retrieved by bitshifting 96 bits to the right
-    ///  Amount is stored in the 12 least significant bytes.
-    ///  The amount is retrieved by taking the 96 least significant bytes
-    ///  and converting them into an unsigned integer.
-    ///  Payable
-    /// @param _addressesAndAmounts Bitwise packed array of contract
-    ///  addresses and amounts
-    function multiCallTightlyPacked(bytes32[] _addressesAndAmounts)
-    payable public returns(bool)
+    /// @notice Most efficient function for Multisigs and other contracts
+    ///  to use; Allows you to make calls to multiple contracts by attaching a
+    ///  byte32 array to the total ETH to be sent; the data includes the
+    ///  receiving address & the amount to be sent stored in a packed bytes32
+    ///  array where the address is in the 20 leftmost bytes, retrieved by
+    ///  bitshifting it 96 bits to the right, and the amount is stored in the 12
+    ///  rightmost bytes, retrieved by taking the 96 rightmost bytes and
+    ///  converting them into an unsigned integer; NOTE: Only calls without data
+    ///  can be made from this contract
+    /// @param _addressesAndAmounts Bitwise packed array of addresses and
+    ///  amounts (denominated in wei) detailing the desired calls
+    function multiCallTightlyPacked(bytes32[] _addressesAndAmounts
+    )payable public returns(bool)
     {
         uint startBalance = this.balance;
         for (uint i = 0; i < _addressesAndAmounts.length; i++) {
@@ -118,12 +129,13 @@ contract MultiSend is Escapable {
         return true;
     }
 
-    /// @notice Call to multiple contracts using two arrays which
-    ///  includes the contract address and the amount.
-    /// @param _addresses Array of contract addresses to call
-    /// @param _amounts Array of amounts to send
-    function multiCall(address[] _addresses, uint[] _amounts)
-    payable public returns(bool)
+    /// @notice Most efficient function when using a normal account; Allows you
+    ///  to call multiple contracts in one transaction by attaching two arrays
+    ///  of data, one array for the addresses and one array for the amounts
+    /// @param _addresses Array of contract addresses to be called
+    /// @param _amounts Array of amounts (in wei) to send to each address
+    function multiCall(address[] _addresses, uint[] _amounts
+    )payable public returns(bool)
     {
         uint startBalance = this.balance;
         for (uint i = 0; i < _addresses.length; i++) {
@@ -134,19 +146,23 @@ contract MultiSend is Escapable {
         return true;
     }
 
-    /// @notice Send ERC20 tokens to multiple contracts 
-    ///  using a byte32 array which includes the address and the amount.
-    ///  Addresses and amounts are stored in a packed bytes32 array.
-    ///  Address is stored in the 20 most significant bytes.
-    ///  The address is retrieved by bitshifting 96 bits to the right
-    ///  Amount is stored in the 12 least significant bytes.
-    ///  The amount is retrieved by taking the 96 least significant bytes
-    ///  and converting them into an unsigned integer.
-    /// @param _token The token to send
-    /// @param _addressesAndAmounts Bitwise packed array of addresses
-    ///  and token amounts
-    function multiERC20TransferTightlyPacked
-    (
+///////////////////
+// Token Functions
+///////////////////
+
+    /// @notice Most efficient function for Multisigs and other Contracts
+    ///  to use; Allows you to send ERC20 tokens to multiple addresses by
+    ///  attaching a byte32 array to the transaction; the data includes the
+    ///  receiving address & the amount of tokens to be sent stored in a packed
+    ///  bytes32 array where the address in the 20 leftmost bytes is retrieved
+    ///  by bitshifting it 96 bits to the right, and the amount stored in the 12
+    ///  rightmost bytes is retrieved by taking the 96 rightmost bytes and
+    ///  converting them into an unsigned integer
+    /// @param _token The token being sent
+    /// @param _addressesAndAmounts Bitwise packed array of receiving addresses
+    ///  amounts (denominated in the smallest unit of the token) detailing the
+    ///  desired ERC20 transfers
+    function multiERC20TransferTightlyPacked(
         ERC20 _token,
         bytes32[] _addressesAndAmounts
     ) public
@@ -159,11 +175,11 @@ contract MultiSend is Escapable {
         }
     }
 
-    /// @notice Send ERC20 tokens to multiple contracts
-    ///  using two arrays which includes the address and the amount.
-    /// @param _token The token to send
-    /// @param _addresses Array of addresses to send to
-    /// @param _amounts Array of token amounts to send
+    /// @notice Most efficient function when using a normal account; Allows you
+    ///  to send ERC20 tokens to multiple addresses by attaching two arrays
+    ///  of data, one array for the addresses and one array for the amounts
+    /// @param _addresses Array of contract addresses to be called
+    /// @param _amounts Array of amounts (in wei) to send to each address
     function multiERC20Transfer(
         ERC20 _token,
         address[] _addresses,
@@ -182,30 +198,36 @@ contract MultiSend is Escapable {
         }
     }
 
-    /// @notice `_safeTransfer` is used internally when transfer funds safely.
+//////////////////////
+// Internal Functions
+//////////////////////
+
+    /// @notice Transfers `_amount` of ETH (denominated in wei) safely to `_to` 
     function _safeTransfer(address _to, uint _amount) internal {
         require(_to != 0);
         _to.transfer(_amount);
     }
 
-    /// @notice `_safeCall` is used internally when call a contract safely.
+    /// @notice Makes a safe call for `_amount` of ETH (in wei) to `_to`
     function _safeCall(address _to, uint _amount) internal {
         require(_to != 0);
         require(_to.call.value(_amount)());
     }
 
-    /// @notice `_safeERC20Transfer` is used internally when
-    ///  transfer a quantity of ERC20 tokens.
-    function _safeERC20Transfer(ERC20 _token, address _to, uint _amount)
-    internal
+
+    /// @notice Transfers `_amount` of ERC20 tokens (denominated in the smallest
+    ///  unit) safely to `_to`
+    function _safeERC20Transfer(ERC20 _token, address _to, uint _amount
+    )internal
     {
         require(_to != 0);
         require(_token.transferFrom(msg.sender, _to, _amount));
     }
 
-    /// @dev Default payable function to not allow sending to contract;
-    ///  remember this does not necesarily prevent the contract
-    ///  from accumulating funds.
+    /// @dev The fallback function is written nicely to stop ether from being
+    ///  sent to the contract but to give back as much gas as possible;
+    ///  remember this does not necessarily prevent the contract from
+    ///  accumulating ETH and tokens via other means
     function () public payable {
         revert();
     }
